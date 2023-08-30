679c94b58a5fc70753ba43d8043473d1ba5e31e0
ANKIT99863 <122007382+ANKIT99863@users.noreply.github.com>
2023-08-30 15:46:20 +0530

    Add files via upload

---
+++ day 2- task 2.js
@@ -0,0 +1,9 @@
+Document and window objects
+1)Documents are the list of files stored in particular folder , where file can be of type any format
+2)The document object represents your web page. If you want to access any element in an HTML page, you always start with accessing the document object.
+Browser Window
+ The window object in JavaScript stands for the current web page that is being seen in a browser window. It gives access to the browser’s methods and attributes as the global object in the browser environment. The window object is the global object in the web browser environment. It represents the current window or frames that the JavaScript code is running in.
+
+A browser window is represented by the window object. The browser produces one window object for the HTML content and one extra window object for each frame when a document contains frames (iframe> elements).
+
+Using the document.defaultView property, one may get a window for a certain document. The window object’s characteristics include global variables. The window object’s methods are known as global functions.
